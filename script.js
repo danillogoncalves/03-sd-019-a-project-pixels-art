9f933f1e1e5f8c229ed32e9b206f53c0afebe95d
danillo <danillogonvalves001@gmail.com>
2021-12-09 20:22:08 -0300

    Tentando fazer o oitavo requisito.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let linha = 5
+let coluna = 5
+let pixelBoard = document.querySelector('#pixel-board');
+for (let l = 0; l < linha; l += 1) {
+    const criaLinha = document.createElement('div');
+    criaLinha.classList.add('line');
+    pixelBoard.appendChild(criaLinha);
+    for (let c = 0; c < coluna; c += 1) {
+        const criaColuna = document.createElement('div');
+        criaColuna.classList.add('pixel')
+        criaLinha.appendChild(criaColuna);
+    }
+}
+
+
+
+
 const blackPalette = document.querySelector('.black');
 const redPalette = document.querySelector('.red');
 const bluePalette = document.querySelector('.blue');
@@ -33,3 +50,5 @@ function selectedPaletteGreen() {
     bluePalette.classList.remove('selected');
     greenPalette.classList.add('selected');
 }
+
+// const pixelBoard = document.querySelectorAll('#pixel-board');
