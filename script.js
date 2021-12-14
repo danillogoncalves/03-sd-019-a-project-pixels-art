0c70a032f9fd1d2fcc6fc45aeaa85fe46bb5e6d7
danillo <danillogonvalves001@gmail.com>
2021-12-14 20:04:46 -0300

    Comentando uma nova maneira de deixar o Math.Random inteiro

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function generateBoard () {
 }
 let color1 = document.querySelector('#color1').style.backgroundColor = 'black';
 let color2 = document.querySelector('#color2').style.backgroundColor = `rgb(${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)})`;
+// let color2 = document.querySelector('#color2').style.backgroundColor = `rgb(${Math.floor((Math.random()*256))}, ${Math.floor((Math.random()*256))}, ${Math.floor((Math.random()*256))})`;
 let color3 = document.querySelector('#color3').style.backgroundColor = `rgb(${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)})`;
 let color4 = document.querySelector('#color4').style.backgroundColor = `rgb(${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)})`;
 
