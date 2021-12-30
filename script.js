a1745e2596b0eb8b565ecbb49be84270b4974566
danillo <danillogonvalves001@gmail.com>
2021-12-30 02:09:51 -0300

    Arrumando o ESLint e o StyleLint.

--- script.js
+++ script.js
@@ -1,93 +1,114 @@
 const buttonGenerateBoard = document.querySelector('#generate-board');
-generateBoard();
-function generateBoard () {
-    let linha = 5;
-    let coluna = 5;
-    let pixelBoard = document.querySelector('#pixel-board');
-    for (let l = 0; l < linha; l += 1) {
-        const criaLinha = document.createElement('div');
-        criaLinha.classList.add('line');
-        pixelBoard.appendChild(criaLinha);
-        for (let c = 0; c < coluna; c += 1) {
-            const criaColuna = document.createElement('div');
-            criaColuna.classList.add('pixel');
-            criaColuna.addEventListener('click', paintPixel);
-            criaLinha.appendChild(criaColuna);
-        }
-    }
-}
-let color1 = document.querySelector('#color1').style.backgroundColor = 'black';
-let color2 = document.querySelector('#color2').style.backgroundColor = `rgb(${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)})`;
-// let color2 = document.querySelector('#color2').style.backgroundColor = `rgb(${Math.floor((Math.random()*256))}, ${Math.floor((Math.random()*256))}, ${Math.floor((Math.random()*256))})`;
-let color3 = document.querySelector('#color3').style.backgroundColor = `rgb(${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)})`;
-let color4 = document.querySelector('#color4').style.backgroundColor = `rgb(${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)}, ${parseInt(Math.random()*256)})`;
+const idPixelBoard = '#pixel-board';
+const idBoardSize = '#board-size';
 
+function createrandomRGB() {
+  return parseInt(Math.random() * 256, 10);
+}
+
+function createRandomColor() {
+  return `rgb(${createrandomRGB()}, ${createrandomRGB()}, ${createrandomRGB()})`;
+}
+
+function paintPixel(e) {
+  const newColor = document.querySelector('.selected').style.backgroundColor;
+  e.target.style.backgroundColor = newColor;
+}
+
+function generateBoard() {
+  const linha = 5;
+  const coluna = 5;
+  const pixelBoard = document.querySelector(idPixelBoard);
+  for (let l = 0; l < linha; l += 1) {
+    const criaLinha = document.createElement('div');
+    criaLinha.classList.add('line');
+    pixelBoard.appendChild(criaLinha);
+    for (let c = 0; c < coluna; c += 1) {
+      const criaColuna = document.createElement('div');
+      criaColuna.classList.add('pixel');
+      criaColuna.addEventListener('click', paintPixel);
+      criaLinha.appendChild(criaColuna);
+    }
+  }
+}
+
+generateBoard();
+
+document.querySelector('#color1').style.backgroundColor = 'black';
+document.querySelector('#color2').style.backgroundColor = createRandomColor();
+// document.querySelector('#color2').style.backgroundColor = `rgb(${Math.floor((Math.random()*256))}, ${Math.floor((Math.random()*256))}, ${Math.floor((Math.random()*256))})`;
+document.querySelector('#color3').style.backgroundColor = createRandomColor();
+document.querySelector('#color4').style.backgroundColor = createRandomColor();
+
+function markColorPalette(e) {
+  document.querySelector('.selected').classList.remove('selected');
+  e.target.classList.add('selected');
+}
 // Mentoria Roberval Filho
 const colorPalette = document.querySelectorAll('.color');
 for (let i = 0; i < colorPalette.length; i += 1) {
-    colorPalette[i].addEventListener('click', markColorPalette)
+  colorPalette[i].addEventListener('click', markColorPalette);
 }
 
-function markColorPalette(e) {
-    document.querySelector('.selected').classList.remove('selected');
-    e.target.classList.add('selected');
+function clearBoard() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.background = 'white';
+  }
 }
-
-function paintPixel (e) {
-    const newColor = document.querySelector('.selected').style.backgroundColor;
-    e.target.style.backgroundColor = newColor;
-}
-
 const buttonClearBoard = document.querySelector('#clear-board');
 buttonClearBoard.addEventListener('click', clearBoard);
-function clearBoard() {
-    const pixels = document.querySelectorAll('.pixel');
-    for (let i = 0; i < pixels.length; i += 1) {
-        pixels[i].style.background = 'white';
+
+function heightWidthGreaterThan50() {
+  document.querySelector(idBoardSize).value = '50';
+  document.querySelector(idPixelBoard).innerHTML = '';
+  const linha = 50;
+  const coluna = 50;
+  const pixelBoard = document.querySelector(idPixelBoard);
+  for (let l = 0; l < linha; l += 1) {
+    const criaLinha = document.createElement('div');
+    criaLinha.classList.add('line');
+    pixelBoard.appendChild(criaLinha);
+    for (let c = 0; c < coluna; c += 1) {
+      const criaColuna = document.createElement('div');
+      criaColuna.classList.add('pixel');
+      criaColuna.addEventListener('click', paintPixel);
+      criaLinha.appendChild(criaColuna);
     }
-}
-function newGenerateBoard() {
-    const heightWidth = parseInt(document.querySelector('#board-size').value);
-    const inputEmpty = document.querySelector('#board-size').value;
-    if (heightWidth < 5 || inputEmpty === '') {
-        alert('Board inválido!');
-    } else if (heightWidth > 50) {
-        document.querySelector('#board-size').value = '50';
-        document.querySelector('#pixel-board').innerHTML = '';
-        let linha = 50;
-        let coluna = 50;
-        let pixelBoard = document.querySelector('#pixel-board');
-        for (let l = 0; l < linha; l += 1) {
-            const criaLinha = document.createElement('div');
-            criaLinha.classList.add('line');
-            pixelBoard.appendChild(criaLinha);
-            for (let c = 0; c < coluna; c += 1) {
-                const criaColuna = document.createElement('div');
-                criaColuna.classList.add('pixel');
-                criaColuna.addEventListener('click', paintPixel);
-                criaLinha.appendChild(criaColuna);
-            }
-        }
-    } else {
-        document.querySelector('#pixel-board').innerHTML = '';
-        let linha = parseInt(document.querySelector('#board-size').value);
-        let coluna = parseInt(document.querySelector('#board-size').value);
-        let pixelBoard = document.querySelector('#pixel-board');
-        for (let l = 0; l < linha; l += 1) {
-            const criaLinha = document.createElement('div');
-            criaLinha.classList.add('line');
-            pixelBoard.appendChild(criaLinha);
-            for (let c = 0; c < coluna; c += 1) {
-                const criaColuna = document.createElement('div');
-                criaColuna.classList.add('pixel');
-                criaColuna.addEventListener('click', paintPixel);
-                criaLinha.appendChild(criaColuna);
-            }
-        }
-    }
+  }
 }
 
-buttonGenerateBoard.addEventListener('click', newGenerateBoard)
+function heightWidthCorrect() {
+  document.querySelector(idPixelBoard).innerHTML = '';
+  const linha = parseInt(document.querySelector(idBoardSize).value, 10);
+  const coluna = parseInt(document.querySelector(idBoardSize).value, 10);
+  const pixelBoard = document.querySelector(idPixelBoard);
+  for (let l = 0; l < linha; l += 1) {
+    const criaLinha = document.createElement('div');
+    criaLinha.classList.add('line');
+    pixelBoard.appendChild(criaLinha);
+    for (let c = 0; c < coluna; c += 1) {
+      const criaColuna = document.createElement('div');
+      criaColuna.classList.add('pixel');
+      criaColuna.addEventListener('click', paintPixel);
+      criaLinha.appendChild(criaColuna);
+    }
+  }
+}
+
+function newGenerateBoard() {
+  const heightWidth = parseInt(document.querySelector(idBoardSize).value, 10);
+  const inputEmpty = document.querySelector(idBoardSize).value;
+  if (heightWidth < 5 || inputEmpty === '') {
+    alert('Board inválido!');
+  } else if (heightWidth > 50) {
+    heightWidthGreaterThan50();
+  } else {
+    heightWidthCorrect();
+  }
+}
+
+buttonGenerateBoard.addEventListener('click', newGenerateBoard);
 // Eu tive ajuda do Lucas Martim Sênior na area de programação Front-End
 // const pixels = document.querySelectorAll('.pixel');
 // for (let i = 0; i < pixels.length; i += 1) {
